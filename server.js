95f0da465d40cd6492ecfe581aa5d27cc76894a1
fsesin <faridsesin@gmail.com>
2022-08-02 17:57:30 -0500

    Prueba

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express')
 
 const app = express()
 
+app.get('/',(req,res)=>{
+    res.send('SERVIDOR CON GIT')
+})
+app.get('/prueba',(req,res)=>{
+    console.log('prueba')
+})
+
 app.listen(8080,()=>{
     console.log('Escuchando al puerto 8080')
 })
